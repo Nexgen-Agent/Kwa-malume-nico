8ae1c68541ef4f9b9e23e78f36a5897d9468960e
Nexgen-Agent <vukonamongwe963@gmail.com>
2025-08-24 23:18:31 +0200

    Update db.py

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,1 +1,12 @@
-ghhg
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+
+DATABASE_URL = "sqlite+aiosqlite:///./backend/malume.db"
+
+# create async engine
+engine = create_async_engine(DATABASE_URL, echo=False, future=True, pool_pre_ping=True)
+
+# async session maker
+AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
+
+Base = declarative_base()
